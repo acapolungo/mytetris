48ebcbf76e8bfad1b2a894e543625df578c69175
Adrien Capolungo <adrien.capolungo@gmail.com>
2022-05-05 16:34:23 +0200

    feat!: add custom gradients & current tetris block

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,83 @@ function App() {
 
       <div className="w-[35rem] h-[42rem] p-[1.75rem] bg-gradient-to-br from-magenta via-purple to-cyan flex justify-between drop-shadow-xl">
         <section className="w-[19.25rem] h-[100%] bg-greybg rounded-md grid grid-cols-11 grid-rows-22 text-white">
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">1</div>
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">2</div>
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">3</div>
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">4</div>
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">5</div>
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">6</div>
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">7</div>
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">8</div>
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">9</div>
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">10</div>
-          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded">11</div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-s-yellow"></div>
+
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+          <div className="w-[1.75rem] h-[1.75rem] flex justify-center items-center border-redbg border-[0.1rem] rounded bg-o-purple"></div>
+
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-l-blue"></div>
+
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
+          <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-i-red"></div>
 
         </section>
         <section className="w-[10.5rem] h-[100%] rounded-sm flexflex-wrap">
           <h1 className="text-[2.5rem] h-[12%] text-cyan">my<span className="font-bold">Tetris</span></h1>
           <section className="w-[100%]">
             <h3 className="w-[10.5rem] text-[1.5rem] text-white">Next</h3>
-            <div className="w-[100%] h-[7rem] bg-greybg rounded-md"></div>
+            <div className="w-[100%] h-[7rem] bg-greybg rounded-md flex flex-wrap justify-center items-center content-center">
+              <div className="w-[100%] flex justify-center">
+                <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+              </div>
+              <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+              <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+              <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.1rem] rounded bg-t-orange"></div>
+            </div>
           </section>
           <section className="w-[100%] mt-[1rem]">
             <h3 className="w-[10.5rem] text-[1.5rem] text-white">Score</h3>
-            <div className="w-[100%] h-[2.625rem] text-[1.5rem] flex items-center bg-greybg rounded-md text-white">125</div>
+            <div className="w-[100%] h-[2.625rem] text-[1.5rem] text-grey-txt flex items-center bg-greybg rounded-md text-white px-[10px]">125</div>
           </section>
         </section>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,14 +16,22 @@ module.exports = {
         'cyan': '#37E5DB',
         'purple': '#9C58EF',
         'greybg': '#303030',
+        'grey-txt': '#666666',
         'cyanbg': '#006484',
         'purplebg': '#6A0A56',
         'white': '#ffffff',
-        'redbg': '#7C7070',
+        'redbg': '#792c2c',
       },
       gridTemplateRows: {
         '22': 'repeat(22, minmax(0, 1fr))',
-      }
+      },
+      backgroundImage: {
+        's-yellow': "linear-gradient(140deg, rgba(248,243,140,1) 0%, rgba(255,220,0,1) 100%)",
+        't-orange': "linear-gradient(140deg, rgba(246,145,79,1) 0%, rgba(230,109,36,1) 100%)",
+        'o-purple': "linear-gradient(140deg, rgba(211,109,171,1) 0%, rgba(174,36,143,1) 100%)",
+        'l-blue': "linear-gradient(140deg, rgba(39,193,242,1) 0%, rgba(0,152,221,1) 100%)",
+        'i-red': "linear-gradient(140deg, rgba(243,121,92,1) 0%, rgba(212,33,41,1) 100%)",
+      },
     }
   },
   plugins: [],
