8af1cb612569fd7f5dd96b95eb989011f2d2f5e2
Adrien Capolungo <adrien.capolungo@gmail.com>
2022-05-26 19:20:21 +0200

    ✨ feat: correction of missing code review (indexes)
    modification of the count by index to mean that we are changing rows by an index
    
    issue:#4

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,17 @@ import './App.css';
 import Cell from './components/Emptycell/Cell';
 
 function emptyGrid() {
-  const rowsOfBoxes = new Array(11).fill({ color: "" });
-  return new Array(22).fill(rowsOfBoxes)
+  const rowsOfBoxes = new Array(11).fill({ color: '' });
+  return new Array(22).fill(rowsOfBoxes);
 }
 
 const checkIfRowBelowExist = (grid: any[], rowIndex: number) => {
   const nextLineRow = grid[rowIndex + 1]
   if (nextLineRow) {
-    return true
+    return true;
   }
-  return false
+  return false;
 }
-
 
 function App() {
   const [tetrisGrid, setTetrisGrid] = useState(emptyGrid());
@@ -31,40 +30,26 @@ function App() {
 
     setTetrisGrid(tetrisGridCopy)
 
-    // if (checkIfRowBelowExist(tetrisGridCopy, rowIndex)) {
-    //   const nextRowSelected = tetrisGridCopy[rowIndex + 1];
-
-    //   if (nextRowSelected[5].color !== "") {
-    //     console.log('la prochaine est colorée')
-    //     setrowIndex(0)
-    //   } else {
-    //     setTimeout(function () {
-    //       rowSelected[5] = { color: '' }
-    //       setTetrisGrid(tetrisGridCopy)
-    //     }, 500)
-    //   }
-    // }
   }, [tetrisGrid, rowIndex])
 
   const removeShapeColor = useCallback(() => {
     const tetrisGridCopy = [...tetrisGrid]
     const rowSelected = [...tetrisGridCopy[rowIndex]];
     if (checkIfRowBelowExist(tetrisGridCopy, rowIndex)) {
-      const nextRowSelected = tetrisGridCopy[rowIndex+1];
+      const nextRowSelected = tetrisGridCopy[rowIndex + 1];
 
-      if (nextRowSelected[5].color !== "") {
-        console.log('la prochaine est colorée')
+      if (nextRowSelected[5].color !== '') {
         setrowIndex(0)
       } else {
         setTimeout(function () {
-          const inactiveBoxStyle = { color: '' }
-          rowSelected[5] = inactiveBoxStyle
-          setTetrisGrid(tetrisGridCopy)
-        }, 480)
+          const inactiveBoxStyle = { color: '', rounded: '' };
+          rowSelected[5] = inactiveBoxStyle;
+          setTetrisGrid(tetrisGridCopy);
+        }, 400)
       }
     }
   }, [tetrisGrid, rowIndex])
-  
+
   useEffect(() => {
     const interval = setInterval(() => {
       if (rowIndex <= 21) {
@@ -79,16 +64,14 @@ function App() {
     return () => clearInterval(interval);
   }, [addShapeColor, removeShapeColor, rowIndex]);
 
-
   return (
 
     <div className="mx-auto h-screen flex justify-center items-center bg-gradient-to-br from-purplebg to-cyanbg">
 
       <div className="w-[35rem] h-[42rem] p-[1.75rem] bg-gradient-to-br from-magenta via-purple to-cyan flex justify-between drop-shadow-xl">
-
-        <section className="w-[19.25rem] h-[100%] bg-greybg grid grid-cols-11 grid-rows-22 text-white">
-          {tetrisGrid.flat().map((box, index) => <Cell key={index} color={box.color} roundedshape={box.rounded} />)}
-        </section>
+          <section className="w-[19.25rem] h-[100%] bg-greybg grid grid-cols-11 grid-rows-22 text-white">
+            {tetrisGrid.flat().map((box, index) => <Cell key={index} color={box.color} roundedshape={box.rounded} />)}
+          </section>
 
         <section className="w-[10.5rem] h-[100%] rounded-sm flexflex-wrap">
           <h1 className="text-[2.5rem] h-[12%] text-cyan">my<span className="font-bold">Tetris</span></h1>
@@ -115,13 +98,3 @@ function App() {
 }
 
 export default App;
-
-
-
-    // 1. Make a shallow copy of the array
-    // 2. Make a shallow copy of the element you want to mutate
-    // 3. Update the property you're interested in
-    // 4. Put it back into our array. N.B. we *are* mutating the array here, but that's why we made a copy first
-    // 5. Set the state to our new copy
-    // 6. SetTimeout the state to remove colored box after 1s
-    // 7. the next line is colored then we start again at rowIndex 0 otherwise we setTimeout and remove the style
--- src/components/Emptycell/Cell.jsx
+++ src/components/Emptycell/Cell.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
-export default function Cell({color, roundedshape}) {
+function bgGradientColor(color) {
+  let className;
+  switch (color) {
+    case 'yellow':
+      className = 'bg-gradient-yellow'
+      break;
+    case 'orange':
+      className = 'bg-gradient-orange'
+      break;
+    case 'purple':
+      className = 'bg-gradient-purple'
+      break;
+    case 'blue':
+      className = 'bg-gradient-blue'
+      break;
+    case 'red':
+      className = 'bg-gradient-red'
+      break;
 
+    default:
+      break;
+  }
+  return className;
+}
+
+export default function Cell({ color, roundedshape }) {
   return (
-    <div className={`w-[1.75rem] h-[1.75rem] border-[0.05rem] border-bordergrid bg-gradient-${color} ${roundedshape}`}></div>
+    <div className={`w-[1.75rem] h-[1.75rem] border-[0.05rem] border-bordergrid ${bgGradientColor(color)} ${roundedshape}`}></div>
   )
 }
