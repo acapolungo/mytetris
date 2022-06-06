0d75bcfdcef107301b77487331c1f985f5d5d4ca
Adrien Capolungo <adrien.capolungo@gmail.com>
2022-06-06 14:34:02 +0200

    🐞 fix: rename action inreducer
    rename reducer action and destructure state variables into functions
    
    issue: #7

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,16 @@ function emptyGrid() {
   return new Array(22).fill(rowsOfCells);
 }
 
+function emptyNextShapeGrid() {
+  const rowsOfCells = new Array(6).fill({ color: '', rounded: '' });
+  return new Array(5).fill(rowsOfCells);
+}
+
 const initialState = {
   tetrisGrid: emptyGrid(),
   currentRowIndex: 0,
-  currentShapeColor: randomColor()
+  currentShapeColor: randomColor(),
+  nextShape: emptyNextShapeGrid()
 }
 
 const reducer = (state = initialState, action) => {
@@ -99,21 +105,18 @@ function App() {
     <div className="mx-auto h-screen flex justify-center items-center bg-gradient-to-br from-purplebg to-cyanbg">
 
       <div className="w-[35rem] h-[42rem] p-[1.75rem] bg-gradient-to-br from-magenta via-purple to-cyan flex justify-between drop-shadow-xl">
-        <section className="w-[19.25rem] h-[100%] bg-greybg grid grid-cols-11 grid-rows-22 text-white">
-          {state?.tetrisGrid.flat().map((box, index) => <Cell key={index} color={box.color} roundedshape={box.rounded} />)}
+        <section className="w-[19.25rem] h-[100%] bg-greybg grid grid-cols-11 grid-rows-22">
+          {state.tetrisGrid.flat().map((box, index) => <Cell key={index} color={box.color} roundedshape={box.rounded} />)}
         </section>
 
         <section className="w-[10.5rem] h-[100%] rounded-sm flexflex-wrap">
           <h1 className="text-[2.5rem] h-[12%] text-cyan">my<span className="font-bold">Tetris</span></h1>
           <section className="w-[100%]">
-            <h3 className="w-[10.5rem] text-[1.5rem] text-white shadow-lg drop-shadow-md-black">Next</h3>
-            <div className="w-[100%] h-[7rem] bg-greybg rounded-md flex flex-wrap justify-center items-center content-center">
-              <div className="w-[100%] flex justify-center">
-                <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.05rem] rounded bg-gradient-orange"></div>
-              </div>
-              <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.05rem] rounded bg-gradient-orange"></div>
-              <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.05rem] rounded bg-gradient-orange"></div>
-              <div className="w-[1.75rem] h-[1.75rem] border-redbg border-[0.05rem] rounded bg-gradient-orange"></div>
+            <h3 className="w-[100%] text-[1.5rem] text-white shadow-lg drop-shadow-md-black">Next</h3>
+            <div className="w-[10.5rem] h-[8.75rem] bg-greybg rounded-md flex flex-wrap justify-center items-center content-center">
+              <section className="w-[100%] h-[100%] bg-greybg grid grid-cols-6 grid-rows-4">
+              {state.nextShape.flat().map((box, index) => <Cell key={index} color={box.color} roundedshape={box.rounded} />)}
+              </section>
             </div>
           </section>
           <section className="w-[100%] mt-[1rem]">
