00fb6ea61c7a5707644670310f895925ac459557
Adrien Capolungo <adrien.capolungo@gmail.com>
2022-07-26 20:46:30 +0200

    🐞 fix: change cell verification fn, add position state
     change of cell verification functions, addition of a position state to pass them in the functions at each keyboard event

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,13 @@ type TetrisState = {
     currentShapeColor: string;
     nextShapeGrid: CellInterface[][];
     nextShapeColor: string;
-    startingPositionInGrid: number;
+    positionInGrid: number;
 }
 
 const randomColor = (): Color => {
     const arrayOfColors: Color[] = ["yellow", "orange", "purple", "blue", "red"];
-    return arrayOfColors[Math.floor(Math.random() * arrayOfColors.length)]
+    const color = arrayOfColors[Math.floor(Math.random() * arrayOfColors.length)]
+    return color
 };
 
 function emptyGrid(): CellInterface[][] {
@@ -44,11 +45,11 @@ const initialState = {
     currentShapeColor: randomColor(),
     nextShapeGrid: emptyNextShapeGrid(),
     nextShapeColor: randomColor(),
-    startingPositionInGrid: 5,
+    positionInGrid: 5,
 }
 
 const reducer = (state: TetrisState, action: ShapeAction): TetrisState => {
-    const { tetrisGrid, currentRowIndex, currentShapeColor, nextShapeColor, startingPositionInGrid } = state;
+    const { tetrisGrid, currentRowIndex, currentShapeColor, nextShapeColor, positionInGrid } = state;
     const tetrisGridCopy = [...tetrisGrid];
     const activeCellStyle = { color: currentShapeColor, rounded: 'rounded-md' }
     const inactiveCellStyle = { color: '', rounded: '' };
@@ -57,7 +58,7 @@ const reducer = (state: TetrisState, action: ShapeAction): TetrisState => {
     switch (action.type) {
         case 'INTRODUCE_SHAPE':
             const firstRow = [...tetrisGridCopy[0]];
-            firstRow[startingPositionInGrid] = { color: nextShapeColor, rounded: 'rounded-md' };
+            firstRow[5] = { color: nextShapeColor, rounded: 'rounded-md' };
             tetrisGridCopy[0] = firstRow;
 
             return {
@@ -65,14 +66,15 @@ const reducer = (state: TetrisState, action: ShapeAction): TetrisState => {
                 currentShapeColor: nextShapeColor,
                 tetrisGrid: tetrisGridCopy,
                 currentRowIndex: 0,
-                nextShapeColor: randomColor()
+                nextShapeColor: randomColor(),
+                positionInGrid: 5,
             }
         case 'MOVE_COLOR_SHAPE':
             const currentRowCopy = [...tetrisGridCopy[currentRowIndex]];
             const nextRowCopy = [...tetrisGridCopy[currentRowIndex + 1]];;
 
-            currentRowCopy[startingPositionInGrid] = inactiveCellStyle;
-            nextRowCopy[startingPositionInGrid] = activeCellStyle;
+            currentRowCopy[positionInGrid] = inactiveCellStyle;
+            nextRowCopy[positionInGrid] = activeCellStyle;
             tetrisGridCopy[currentRowIndex] = currentRowCopy;
             tetrisGridCopy[currentRowIndex + 1] = nextRowCopy;
 
@@ -83,27 +85,27 @@ const reducer = (state: TetrisState, action: ShapeAction): TetrisState => {
             }
         case 'MOVE_LEFT':
             const currentRowCopy2 = [...tetrisGridCopy[currentRowIndex]];
-            currentRowCopy2[startingPositionInGrid] = inactiveCellStyle;
-            const newPositionToTheLeft = startingPositionInGrid - movementCounter
-            currentRowCopy2[startingPositionInGrid - movementCounter] = activeCellStyle;
+            currentRowCopy2[positionInGrid] = inactiveCellStyle;
+            const newPositionToTheLeft = positionInGrid - movementCounter
+            currentRowCopy2[newPositionToTheLeft] = activeCellStyle;
             tetrisGridCopy[currentRowIndex] = currentRowCopy2;
 
             return {
                 ...state,
                 tetrisGrid: tetrisGridCopy,
-                startingPositionInGrid: newPositionToTheLeft,
+                positionInGrid: newPositionToTheLeft,
             }
         case 'MOVE_RIGHT':
             const currentRowCopy3 = [...tetrisGridCopy[currentRowIndex]];
-            currentRowCopy3[startingPositionInGrid] = inactiveCellStyle;
-            const newPositionToTheRight = startingPositionInGrid + movementCounter
-            currentRowCopy3[startingPositionInGrid + movementCounter] = activeCellStyle;
+            currentRowCopy3[positionInGrid] = inactiveCellStyle;
+            const newPositionToTheRight = positionInGrid + movementCounter
+            currentRowCopy3[newPositionToTheRight] = activeCellStyle;
             tetrisGridCopy[currentRowIndex] = currentRowCopy3;
 
             return {
                 ...state,
                 tetrisGrid: tetrisGridCopy,
-                startingPositionInGrid: newPositionToTheRight,
+                positionInGrid: newPositionToTheRight,
             }
         default:
             throw new Error(`Unknown action type: ${action.type}`);
@@ -113,38 +115,34 @@ const reducer = (state: TetrisState, action: ShapeAction): TetrisState => {
 const checkIfRowBelowExist = (grid: CellInterface[][], rowIndex: number): CellInterface[] | undefined => grid[rowIndex + 1];
 const checkIfRowBelowIsTaken = (grid: CellInterface[][], rowIndex: number, position: number): boolean => grid[rowIndex + 1][position].color === '';
 
-const checkIfLeftMoveExist = (grid: CellInterface[][], rowIndex: number, move: number): boolean | undefined => {
-    const findIndexOfFirstColoredShape = grid[rowIndex].findIndex(element => element.color !== '')
-    const indexToTheLeft = findIndexOfFirstColoredShape - move
-    return indexToTheLeft >= 0
+const checkIfLeftMoveExist = (grid: CellInterface[][], rowIndex: number, position: number) => {
+    return grid[rowIndex][position - 1] !== undefined;
 }
-const checkIfLeftCellIsTaken = (grid: CellInterface[][], rowIndex: number, move: number): boolean => {
-    const findIndexOfFirstColoredShape = grid[rowIndex].findIndex(element => element.color !== '')
-    const indexToTheLeft = findIndexOfFirstColoredShape - move
-    return grid[rowIndex][indexToTheLeft].color === '';
+const checkIfLeftCellIsTaken = (grid: CellInterface[][], rowIndex: number, position: number): boolean | undefined => {
+    return grid[rowIndex][position - 1].color === '';
 };
 
-const checkIfRightMoveExist = (grid: CellInterface[][], rowIndex: number, move: number): boolean | undefined => {
-    const findIndexOfFirstColoredShape = grid[rowIndex].findIndex(element => element.color !== '')
-    const indexToTheRight = findIndexOfFirstColoredShape + move
-    return indexToTheRight < 11
+const checkIfRightMoveExist = (grid: CellInterface[][], rowIndex: number, position: number): boolean => {
+    return grid[rowIndex][position + 1] !== undefined
 }
-const checkIfRightCellIsTaken = (grid: CellInterface[][], rowIndex: number, move: number): boolean => {
-    const findIndexOfFirstColoredShape = grid[rowIndex].findIndex(element => element.color !== '')
-    const indexToTheRight = findIndexOfFirstColoredShape + move
-    return grid[rowIndex][indexToTheRight].color === '';
+const checkIfRightCellIsTaken = (grid: CellInterface[][], rowIndex: number, position: number): boolean | undefined => {
+    return grid[rowIndex][position + 1].color === "";
 };
 
 const checkIfNextMovePossible = (grid: CellInterface[][], rowIndex: number, position: number): boolean | undefined => {
     return checkIfRowBelowExist(grid, rowIndex) && checkIfRowBelowIsTaken(grid, rowIndex, position)
 }
 
-const checkGoToLeft = (grid: CellInterface[][], rowIndex: number): boolean | undefined => {
-    return checkIfLeftMoveExist(grid, rowIndex, 1) && checkIfLeftCellIsTaken(grid, rowIndex, 1)
+const checkGoToLeft = (grid: CellInterface[][], rowIndex: number, position: number): boolean | undefined => {
+    // console.log('test si une case est libre', checkIfLeftMoveExist(grid, rowIndex, 1))
+    // console.log('test si une case est colorée', checkIfLeftCellIsTaken(grid, rowIndex, 1))
+    return checkIfLeftMoveExist(grid, rowIndex, position) && checkIfLeftCellIsTaken(grid, rowIndex, position)
 }
 
-const checkGoToRight = (grid: CellInterface[][], rowIndex: number): boolean | undefined => {
-    return checkIfRightMoveExist(grid, rowIndex, 1) && checkIfRightCellIsTaken(grid, rowIndex, 1)
+const checkGoToRight = (grid: CellInterface[][], rowIndex: number, position: number): boolean | undefined => {
+    // console.log('test si une case est libre', checkIfRightMoveExist(grid, rowIndex, 1))
+    // console.log('test si une case est colorée', checkIfRightCellIsTaken(grid, rowIndex, 1))
+    return checkIfRightMoveExist(grid, rowIndex, position) && checkIfRightCellIsTaken(grid, rowIndex, position)
 }
 
 function App(): JSX.Element {
@@ -157,9 +155,8 @@ function App(): JSX.Element {
     }, [])
 
     const moveShape = useCallback(() => {
-        const { tetrisGrid, currentRowIndex, startingPositionInGrid } = state;
-
-        if (checkIfNextMovePossible(tetrisGrid, currentRowIndex, startingPositionInGrid)) {
+        const { tetrisGrid, currentRowIndex, positionInGrid } = state;
+        if (checkIfNextMovePossible(tetrisGrid, currentRowIndex, positionInGrid)) {
             dispatch({
                 type: 'MOVE_COLOR_SHAPE'
             })
@@ -169,9 +166,9 @@ function App(): JSX.Element {
     }, [introduceShape, state])
 
     const moveShapeLeft = useCallback(() => {
-        const { tetrisGrid, currentRowIndex } = state;
+        const { tetrisGrid, currentRowIndex, positionInGrid } = state;
 
-        if (checkGoToLeft(tetrisGrid, currentRowIndex)) {
+        if (checkGoToLeft(tetrisGrid, currentRowIndex, positionInGrid)) {
             dispatch({
                 type: 'MOVE_LEFT'
             })
@@ -181,9 +178,9 @@ function App(): JSX.Element {
     }, [state])
 
     const moveShapeRight = useCallback(() => {
-        const { tetrisGrid, currentRowIndex } = state;
+        const { tetrisGrid, currentRowIndex, positionInGrid } = state;
 
-        if (checkGoToRight(tetrisGrid, currentRowIndex)) {
+        if (checkGoToRight(tetrisGrid, currentRowIndex, positionInGrid)) {
             dispatch({
                 type: 'MOVE_RIGHT'
             })
@@ -193,9 +190,10 @@ function App(): JSX.Element {
     }, [state])
 
     const moveShapeDown = useCallback(() => {
-        const { tetrisGrid, currentRowIndex, startingPositionInGrid } = state;
 
-        if (checkIfNextMovePossible(tetrisGrid, currentRowIndex, startingPositionInGrid)) {
+        const { tetrisGrid, currentRowIndex, positionInGrid } = state;
+
+        if (checkIfNextMovePossible(tetrisGrid, currentRowIndex, positionInGrid)) {
             dispatch({
                 type: 'MOVE_COLOR_SHAPE'
             })
@@ -239,9 +237,9 @@ function App(): JSX.Element {
         };
         document.addEventListener("keydown", listener);
         return () => {
-            document.removeEventListener("keydown", listener);
+            document.removeEventListener("keydown", listener);;
         };
-    }, [moveShapeLeft, moveShapeRight]);
+    }, [moveShapeLeft, moveShapeRight, moveShapeDown, moveShape]);
 
     return (
 
